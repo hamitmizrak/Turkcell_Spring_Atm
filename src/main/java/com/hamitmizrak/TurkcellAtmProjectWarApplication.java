1c35cee0de11117a4b67b5c9685b1059aea593a7
hamitmizrak <hamitmizrak@gmail.com>
2022-07-22 10:23:25 +0300

    all config success

---
+++ src/main/java/com/hamitmizrak/TurkcellAtmProjectWarApplication.java
@@ -0,0 +1,18 @@
+package com.hamitmizrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// websecurity exclude
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class })
+public class TurkcellAtmProjectWarApplication {
+	
+	public static void main(String[] args) {
+		System.setProperty("spring.devtools.restart.enabled", "true");
+		System.setProperty("java.awt.headless", "false"); // Disables headless
+		SpringApplication.run(TurkcellAtmProjectWarApplication.class, args);
+	}
+	
+}
---
+++ src/main/java/com/hamitmizrak/sql/AllSqlProject.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE turkcell_db
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
